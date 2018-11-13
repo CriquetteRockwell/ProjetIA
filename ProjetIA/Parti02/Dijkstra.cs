9b7222118c64c021e136a47a75c9ead0acf4874b
Clara Sorita <claraso@hotmail.fr>
2018-11-13 16:12:52 +0100

    Ajout Point et Dijkstra

---
+++ ProjetIA/Parti02/Dijkstra.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parti02
+{
+    class Dijkstra
+    {
+        public List<Point> Graphe { get; set; }
+        public List<Point> Ouvert { get; set; }
+        public List<Point> Ferme { get; set; }
+        public Point Premier { get; set; }
+        public Point Dernier { get; set; }
+
+        public Dijkstra(List<Point> graphe, Point p,Point d)
+        {
+            Graphe = graphe;
+            Premier = p;
+            Dernier = d;
+        }
+
+        public int Rechercher()
+        {
+            int dist = 0;
+            Ouvert.Add(Premier);
+            Ouvert[0].DistParcourue = 0;
+            //Ajouter points suivant dans ouverts
+            while (!EstPresentDansListe(Dernier,Ferme))
+            {
+                int plusPetitChemin = Ouvert[0].DistParcourue + Ouvert[0].DistMin;
+                int ptChoisi = 0;
+
+                for(int i=0;i<Ouvert.Count();i++)
+                {
+                    if(Ouvert[i].DistMin==-1)
+                        //On enlève les points cul de sac
+                    {
+                        Ferme.Add(Ouvert[i]);
+                        Ouvert.RemoveAt(i);
+                    }
+                    else
+                    // On cherche le point avec le poids le moins fort
+                    {
+                        if(Ouvert[i].DistParcourue + Ouvert[i].DistMin < plusPetitChemin)
+                        {
+                            plusPetitChemin = Ouvert[i].DistParcourue + Ouvert[i].DistMin;
+                            ptChoisi = i;
+                        }
+                    }
+                }
+            }
+            return dist;
+        }
+
+        public bool EstPresentDansListe(Point P, List<Point> Points)
+        {
+            bool test = false;
+            for (int i = 0; (i < Points.Count) && (!test); i++)
+            {
+                if (P.Nom == Points[i].Nom)
+                    test = true;
+            }
+            return test;
+        }
+    }
+}
---
+++ ProjetIA/Parti02/Point.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parti02
+{
+    class Point
+    {
+        public string Nom { get; set; }
+        public List<Point> PA { get; set; } //Points Adjacents
+        public List<int> DistPA { get; set; }
+        public int DistParcourue { get; set; }
+        public Point PtMin;
+        public int DistMin;
+        public Point(){}
+        public Point(string n, List<Point> pa, List<int> dpa)
+        {
+            Nom = n;
+            PA = pa;
+            DistPA = dpa;
+            DistParcourue = 100000;
+            this.PointPlusProche();
+        }
+        //Il faudra retirer les point fermés
+        public void PointPlusProche()
+        {
+            if(PA.Count()!=0)
+            {
+                int indexmin = 0;
+                for (int i = 1; i < PA.Count(); i++)
+                {
+                    if (DistPA.ElementAt(i) < DistMin)
+                    {
+                        indexmin = i;
+                    }
+                }
+                PtMin = PA.ElementAt(indexmin);
+                DistMin = DistPA.ElementAt(indexmin);
+            }
+            else
+            {
+                DistMin = -1;
+            }
+            
+            
+        }
+
+        public void RetirerPoint(Point pt)
+        {
+            int index = PA.IndexOf(pt);
+            PA.Remove(pt);
+            DistPA.RemoveAt(index);
+        }
+
+    }
+}
